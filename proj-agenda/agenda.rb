5024f3046b4b486bda62f27b9f6f3ce1928b03d0
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-05 18:51:15 -0300

    ver contato

--- proj-agenda/agenda.rb
+++ proj-agenda/agenda.rb
@@ -18,6 +18,29 @@ def todos_contatos
     puts "----------------------------"
 end
 
+
+def adicionar_contato
+    print "Nome: "
+    nome = gets.chomp
+    print "Telefone: "
+    telefone = gets.chomp
+
+    #atribuir a resposta do usuário para dentro de um array
+    @agenda << {nome: nome, telefone: telefone}
+end
+
+def ver_contato
+    print "Qual contato você deseja?"
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        #contato está incluido na lista?
+        if contato[:nome].downcase.include?(nome.downcase)
+            puts "#{contato[:nome]} - #{contato[:telefone]}"
+        end 
+    end 
+end 
+
 loop do
 
 puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
@@ -29,5 +52,9 @@ puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. R
         break
     when codigo == 1
         todos_contatos
+    when codigo == 2 
+        adicionar_contato
+    when codigo == 3
+        ver_contato
     end
 end
